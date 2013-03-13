2b68b5d0bde13352ecfdd6512d220d03880fa281
Rich FitzJohn <rich.fitzjohn@gmail.com>
2013-03-13 19:07:37 +1100

    Simplify getting parameters, harmonise with C version, fix path issues.

--- R/falster.R
+++ R/falster.R
@@ -8,62 +8,44 @@
 
 ## The files falster/params.r and falster/growthModel.r are unchanged
 ## from versions recieved about 2012-10-29.
-make.falster <- function() {
-  e <- new.env()
-  source("falster/params.r", local=e)
-  source("falster/growthModel.r", local=e)
-  ## Not used.
-  ## source("falster/plots.r", local=e)
-  e$traits <- list(lma=1.11E-01, rho=608, hmat=20)
-  ## These are done in main.R, but skipping for now.
-  ## e$p.c_r1 = 0.75
-  ## e$p.c_r2 = 10
+make.falster.generator <- function(path) {
+  force(path)
+  function() {
+    e <- new.env()
+    source(file.path(path, "falster/params.r"), local=e)
+    source(file.path(path, "falster/growthModel.r"), local=e)
+    ## Not used.
+    ## source("falster/plots.r", local=e)
+    e$traits <- list(lma=1.11E-01, rho=608, hmat=20)
 
-  ## The next thing that I need is a translation table for the different
-  ## parameters.
+    ## Reset a few parameters to revert back to C++ defaults
+    e$traits$lma <- 0.1978791
+    e$traits$hmat <- 16.5958691
+    e$p.c_bio <- 2.45e-2
+    e$p.a4 <- 0.0286
+    e$p.theta <- 4669
 
-  ## My version:
-  e$tr.pars <-
-    c("A0"=NA, # missing -- only used in LRC calc?
-      "Y"="p.Y",
-      "a1"="p.a1",
-      "a2"="p.a2",
-      "a3"="p.a3",
-      "a4"="p.a4",
-      "b"="p.b",
-      "b1"="p.B1",
-      "b2"="p.B2",
-      "b4"="p.B4",
-      "c_Rl"="p.c_Rl",
-      "c_Rr"="p.c_Rr",
-      "c_Rs"="p.c_Rs",
-      "c_acc"="p.c_acc",
-      "c_bio"="p.c_bio",
-      "c_p1"="p.c_p1",
-      "c_p2"="p.c_p2",
-      "c_r1"="p.c_r1",
-      "c_r2"="p.c_r2",
-      "hmat"=NA,  # traits$hmat
-      "kb"="p.k_b",
-      "kr"="p.k_r",
-      "lma"=NA, # traits$lma
-      "ml0"=NA, # computed, from s
-      "n"="p.eta",
-      "nc"=NA, # I compute this -- not sure if Daniel does this too
-      "rho"=NA, # traits$rho
-      "s"=NA,   # traits$s
-      "theta"="p.theta",
-      "v"="p.n_area")
-  e
+    ## Function to return current values of parameters.
+    ## All parameters can be translated as x -> p.x, where 'x' is the
+    ## name in the C++ version.
+    vars <- c("eta", "theta", "a1", "B1", "a2", "B2", "a3", "b",
+              "n_area", "c_Rl", "c_Rr", "c_Rs", "c_Rb", "Y",
+              "c_bio", "a4", "B4", "k_b", "k_r", "c_p1", "c_p2",
+              "c_acc", "c_r1", "c_r2")
+    vars.msg <- list("Pi_0"=NA, "c_s0"=NA, "c_d0"=NA, "c_d1"=NA,
+                     "c_d2"=NA, "c_d3"=NA)
+    get.pars <- function() {
+      vars.traits <- list(lma=e$traits$lma, rho=e$traits$rho,
+                          hmat=e$traits$hmat, s=NA) # not set.
+      ret <- lapply(sprintf("p.%s", vars), get, e)
+      names(ret) <- vars
+      c(vars.traits, ret, vars.msg)
+    }
+    e$get_parameters <- get.pars
+
+    e
+  }
 }
 
-## These (a5, B5) are used for computing the diameter (via Diameter).
-## Not sure if/how these are used though.
-## "p.a5" # not sure
-## "p.B5" # not sure
-
-## Other queries/comments
-## c_r1, c_r2 (used in seed production) are altered in main.R to
-## different values than we have.
-##
-## hmat appears twice in Daniels code, but once in mine.
+make.falster <- make.falster.generator(getwd())
+rm(make.falster.generator)
