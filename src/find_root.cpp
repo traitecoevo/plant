31061fbb7c34156befb0e3f76cee2f7e34e94c88
Rich FitzJohn <rich.fitzjohn@gmail.com>
2013-03-15 11:37:41 +1100

    Class-based version of the GSL root finder.

--- src/find_root.cpp
+++ src/find_root.cpp
@@ -4,49 +4,59 @@
 
 namespace util {
 
-double find_root(DFunctor *f, double x_lo, double x_hi) {
-  double atol = 0.0, rtol = 0.001;
-  int iter = 0, max_iter = 100;
-
-  gsl_function F;
+RootFinder::RootFinder(double atol, double rtol, int max_iterations)
+  : atol(atol), rtol(rtol), max_iterations(max_iterations) {
   F.function = &helper_functor;
+  solver = gsl_root_fsolver_alloc(gsl_root_fsolver_brent);
+}
+
+RootFinder::~RootFinder() {
+  // clean up
+  gsl_root_fsolver_free(solver);
+}
+
+// TODO: make sure naming and structure is consistent with the
+// integrate wrapper, which is basically doing the same thing.
+double RootFinder::root(DFunctor *f, 
+			double x_min, double x_max) {
   F.params = (void*)(f);
-  
-  gsl_root_fsolver *s = gsl_root_fsolver_alloc(gsl_root_fsolver_brent);
-  gsl_root_fsolver_set(s, &F, x_lo, x_hi);
+  gsl_root_fsolver_set(solver, &F, x_min, x_max);
+  iterations = 0;
 
   int status;
   double r;
   do {
-    iter++;
-    status = gsl_root_fsolver_iterate(s);
-    r = gsl_root_fsolver_root(s);
-    x_lo = gsl_root_fsolver_x_lower(s);
-    x_hi = gsl_root_fsolver_x_upper(s);
-    status = gsl_root_test_interval(x_lo, x_hi, atol, rtol);
-  } while (status == GSL_CONTINUE && iter < max_iter);
-  
-  // clean up
-  gsl_root_fsolver_free(s);
+    iterations++;
+    status = gsl_root_fsolver_iterate(solver);
+    r = gsl_root_fsolver_root(solver);
+    x_min = gsl_root_fsolver_x_lower(solver);
+    x_max = gsl_root_fsolver_x_upper(solver);
+    status = gsl_root_test_interval(x_min, x_max, atol, rtol);
+  } while (status == GSL_CONTINUE && iterations < max_iterations);
+
+  last_error = x_max - x_min;
 
   // TODO: Detect false convergence and throw.
   // Rprintf("Converged with status %d in %d iterations (err: %2.5f)\n", 
-  // 	  status, iter, x_hi - x_lo);
-
+  // 	  status, iterations, last_error);
 
   return r;
 }
 
 namespace test {
 
-double test_find_root(std::vector<double> pars, 
-		      double x_lo, double x_hi) {
+double test_find_root(std::vector<double> pars,
+		      double x_min, double x_max) {
   if ( (int)pars.size() != 3 )
     Rf_error("Expected parameters of length 3");
   Quadratic obj(pars[0], pars[1], pars[2]);
-  
   Functor<test::Quadratic, &test::Quadratic::mytarget> fun(&obj);
-  return find_root(&fun, x_lo, x_hi);
+
+  // Control parameters for the integrator
+  const double atol = 1e-6, rtol = 1e-6;
+  const int max_iterations = 1000;
+  RootFinder root(atol, rtol, max_iterations);
+  return root.root(&fun, x_min, x_max);
 }
 
 }
--- src/find_root.h
+++ src/find_root.h
@@ -10,10 +10,64 @@
 
 namespace util {
 
-double find_root(DFunctor *f, double x_lo, double x_hi);
+// TODO: not totally clear what the best name is here for the class.
+// Should be a noun (as is Spline, Solver, etc).
+class RootFinder {
+public:
+  RootFinder(double atol, double rtol, int max_iterations);
+  ~RootFinder();
+  double root(DFunctor *f, double x_min, double x_max);
+
+private:
+  // Prevent copying and assignment
+  // RootFinder(const RootFinder other&);
+  // RootFinder& operator=(const RootFinder rhs=);
+
+  // TODO: Here, and elsewhere, these are abbreviations.
+  double atol, rtol;
+  int max_iterations;
+  int iterations;
+
+  gsl_function F;
+  gsl_root_fsolver *solver;
+
+  // I had something like this last time.
+  double last_error;
+};
+
+// This may be useful as more often than not we are *actually*
+// interested in where the target function takes a particular value
+// `value`, rather than when it is zero.
+// 
+// With this we can do
+// FunctorRoot<MyClass,&MyClass::target> f(this, 2.3);
+// and then pass &f as the functor to the root finder:
+// 
+// RootFinder root_finder(atol, rtol, max_iterations);
+// root_finder.find_root(&f, x_min, x_max)
+// 
+// This could probaby be easier if we made a functioniod so that
+// something like
+// 
+// root_finder.find_root(FunctorRoot<MyClass,&MyClass::target>(this, 2.3),
+//                       x_min, x_max)
+//
+// worked, but that's actually not much clearer.
+
+// template <class T, double(T::*target)(double)>
+// class FunctorRoot : public DFunctor {
+//   FunctorRoot(T *obj, value) : obj(obj), value(value) {}
+//   virtual double operator()(double x) {
+//     return (obj->*target)(x) - value;
+//   }
+  
+// private:
+//   T* obj;
+//   double value;
+// }
 
 namespace test {
-double test_find_root(std::vector<double> pars, double x_lo, double x_hi);
+double test_find_root(std::vector<double> pars, double x_min, double x_max);
 }
 
 }
