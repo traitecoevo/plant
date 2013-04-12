45bee7ccaaeda7d9b98d832271bb041ec2da282c
Rich FitzJohn <rich.fitzjohn@gmail.com>
2013-04-12 16:43:23 +1000

    New expect_that helper functions involving ranges.
    Test that a number is in a range, or that a range contains a number,
    or that a number is less than a number, or greater than a number.

--- tests/helper-tree.R
+++ tests/helper-tree.R
@@ -32,6 +32,43 @@ if ( !exists("Spline") ) {
 
 source("../R/falster.R", chdir=TRUE)
 
+## New expect_that helper functions; test that a number is in a range,
+## or that a range contains a number.
+is_within_interval <- function(lower, upper) {
+  if ( missing(upper) && length(lower) == 2 ) {
+    upper <- lower[[2]]
+    lower <- lower[[1]]
+  }
+  if ( lower >= upper )
+    stop("lower must be smaller than upper")
+  err <- paste0("is not within range [", lower, ", ", upper, "]")
+  function(actual) {
+    expectation(actual > lower && actual < upper, err)
+  }
+}
+
+contains <- function(value) {
+  function(range) {
+    if ( length(range) != 2 )
+      stop("Expected a vector of length 2")
+    if ( range[[1]] >= range[[2]] )
+      stop("Expected that range[1] is smaler than range[2]")
+    expectation(value > range[[1]] && value < range[[2]],
+                paste("does not contain", value))
+  }
+}
+
+is_greater_than <- function(value) {
+  function(actual)
+    expectation(actual > value, paste("is not greater than", value))
+}
+
+is_less_than <- function(value) {
+  function(actual)
+    expectation(actual < value, paste("is not less than", value))
+}
+
+
 ## This is here to stop a really weird bug (not our fault(?)) around
 ## finalisers.
 gc()
