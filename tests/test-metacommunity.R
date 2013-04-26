d5adc61f4a49e35086545c2f4a5dea47a8c4f49b
Rich FitzJohn <rich.fitzjohn@gmail.com>
2013-04-26 17:23:39 +1000

    Test basic ODE properties of Metacommunity

--- tests/test-metacommunity.R
+++ tests/test-metacommunity.R
@@ -11,10 +11,17 @@ expect_that(sys$size, equals(p$parameters$n_patches))
 n <- matrix(0, p$size, sys$size)
 expect_that(sys$n_individuals, equals(n))
 
+## Age of system should be zero
+expect_that(sys$age, equals(0.0))
+
+## and we should have no ode variables:
+expect_that(sys$ode_size, equals(0))
+
 ## Put a single individual in the 3rd patch:
 n[3] <- 1
 sys$add_plants(n)
 expect_that(sys$n_individuals, equals(n))
+expect_that(sys$ode_size, equals(sum(n)*3))
 
 rm(sys)
 gc()
