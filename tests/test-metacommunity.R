27deca622c9957232fd1c8e6a01a9152a94f9df7
Rich FitzJohn <rich.fitzjohn@gmail.com>
2013-04-29 11:00:22 +1000

    Tests for Metapopulation derivatives.

--- tests/test-metacommunity.R
+++ tests/test-metacommunity.R
@@ -24,13 +24,12 @@ sys$add_plants(n)
 expect_that(sys$n_individuals, equals(n))
 expect_that(sys$ode_size, equals(sum(n)*3))
 
-patch <- sys[[idx]]
-expect_that(patch$n_individuals, equals(1)) # 1 individual
-expect_that(patch$size, equals(p$size))     # 1 species
-expect_that(patch[[1]]$size, equals(1))     # 1 plant
+expect_that(sys[[idx]]$n_individuals, equals(1)) # 1 individual
+expect_that(sys[[idx]]$size, equals(p$size))     # 1 species
+expect_that(sys[[idx]][[1]]$size, equals(1))     # 1 plant
 
 y <- new(Plant, p[[1]])$ode_values
-expect_that(sys$ode_values, is_identical_to(patch$ode_values))
+expect_that(sys$ode_values, is_identical_to(sys[[idx]]$ode_values))
 expect_that(sys$ode_values, is_identical_to(y))
 
 sys$clear()
@@ -43,21 +42,23 @@ expect_that(sys$n_individuals, equals(n))
 expect_that(sys$ode_values,
             is_identical_to(rep(y, sys$size)))
 
-## TODO: Currently fails
-##   sys$derivs(sys$age, sys$ode_values)
-##
-## but so does this, so the problem here is in Patch, constructing a
-## spline that is too fine over a very short height implied by the
-## seed?
-##   patch <- sys[[1]]
-##   patch$derivs(patch$age, patch$ode_values)
-##
-## Also note that this fails when a patch is empty (should be ok).
+## Compare derivative calculations for full system and patch.
+patch <- new(Patch, p)
+patch$add_seeds(1)
+expect_that(sys$ode_values,
+            is_identical_to(rep(patch$ode_values, sys$size)))
+
+expect_that(sys$derivs(sys$age, sys$ode_values),
+            is_identical_to(rep(patch$derivs(patch$age, patch$ode_values),
+                                sys$size)))
+
+expect_that(sys[[idx]]$derivs(sys[[idx]]$age, sys[[idx]]$ode_values),
+            is_identical_to(patch$derivs(patch$age, patch$ode_values)))
 
 ## TODO: Add a r_set_mass_leaf function that can be used to set leaf
-## mas like Species::r_set_mass_leaf and Patch::r_set_mass_leaf.  Will
-## be easier if we have a get_mass_leaf and if the existing functions
-## are modified to take matrices/lists as appropriate.
+## mass like Species::r_set_mass_leaf and Patch::r_set_mass_leaf.
+## Will be easier if we have a get_mass_leaf and if the existing
+## functions are modified to take matrices/lists as appropriate.
 
 rm(sys)
 gc()
