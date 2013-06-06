657f2b6b1f1389359f52c1f645166f455da8d0c2
Rich FitzJohn <rich.fitzjohn@gmail.com>
2013-06-06 15:17:23 +1000

    Tests for doing a full EBT run.
    Tests are very rambly at present.

--- tests/test-ebt.R
+++ tests/test-ebt.R
@@ -23,7 +23,7 @@ test_that("Empty CohortSchedule", {
 expect_that(ebt$cohort_schedule <- new(CohortSchedule, sched$n.species + 1),
             throws_error())
 
-t <- seq(0, 10, length=11)
+t <- seq(0, 5, length=11)
 sched$set_times(t, 1)
 ebt$cohort_schedule <- sched
 
@@ -57,6 +57,67 @@ test_that("EBT reset successful", {
   expect_that(ebt$patch$ode_size, equals(0))
   expect_that(ebt$patch$n_individuals, equals(0))
 })
+ebt$seed_rain <- seed_rain(r)
+
+## Run the whole schedule, and compare with a Patch run manually.
+## TODO: This is really very slow at the moment...
+patch <- new(PatchCohortTop, p)
+patch$seed_rain <- ebt$seed_rain
+species.index <- 1
+sched$reset()
+## Advance to time 't', then add species.
+tt.p <- hh.p <- NULL
+while (sched$remaining > 0) {
+  patch$run_deterministic(sched$next_time)
+  patch$add_seedling(species.index)
+  sched$pop()
+  tt.p <- c(tt.p, patch$time)
+  hh.p <- c(hh.p, list(patch$height[[species.index]]))
+}
+n <- length(hh.p[[length(hh.p)]])
+hh.p <- t(sapply(hh.p, function(x) c(x, rep(NA, n-length(x)))))
+
+## Currently reset
+## ebt <- new(EBT, p)
+## ebt$seed_rain <- seed_rain(r)
+## sched$reset()
+## ebt$cohort_schedule <- sched
+ebt$reset()
+ebt$seed_rain <- seed_rain(r)
+tt.e <- hh.e <- NULL
+repeat {
+  if (inherits(try(ebt$run_next(), silent=TRUE), "try-error"))
+    break
+  tt.e <- c(tt.e, ebt$time)
+  hh.e <- c(hh.e, list(ebt$patch$height[[species.index]]))
+}
+n <- length(hh.e[[length(hh.e)]])
+hh.e <- t(sapply(hh.e, function(x) c(x, rep(NA, n-length(x)))))
+
+## However, *this* works:
+ebt$reset()
+ebt$seed_rain <- ebt$seed_rain
+tt.e2 <- hh.e2 <- NULL
+repeat {
+  if (inherits(try(ebt$run_next(), silent=TRUE), "try-error"))
+    break
+  tt.e2 <- c(tt.e2, ebt$time)
+  hh.e2 <- c(hh.e2, list(ebt$patch$height[[species.index]]))
+}
+n <- length(hh.e2[[length(hh.e2)]])
+hh.e2 <- t(sapply(hh.e2, function(x) c(x, rep(NA, n-length(x)))))
+
+expect_that(hh.e, equals(hh.e2))
+expect_that(hh.p, equals(hh.e2))
+
+## So - unlikely to be caused by the seeds (which are regenerated) or
+## the environment (which is regenerated).  Not sure.  *Could* be
+## something to do with the step size calculations.  Or the estimated
+## slope?
+if (interactive()) {
+  matplot(tt.p, hh.p, type="o", col="black", pch=1, lty=1)
+  matpoints(tt.e, hh.e, col="red", cex=.5, pch=1, type="o", lty=1)
+}
 
 rm(ebt)
 gc()
