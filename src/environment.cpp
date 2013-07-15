c6505ed1164a7a369ffeb7463af4d64139eafe1f
Rich FitzJohn <rich.fitzjohn@gmail.com>
2013-07-15 11:33:16 +1000

    Prefer empty() over size() == 0

--- src/environment.cpp
+++ src/environment.cpp
@@ -55,7 +55,7 @@ void Environment::clear() {
 }
 
 double Environment::seed_rain_rate() const {
-  if (seed_rain.size() == 0)
+  if (seed_rain.empty())
     ::Rf_error("Cannot get seed rain for empty environment");
   return seed_rain[seed_rain_index];
 }
