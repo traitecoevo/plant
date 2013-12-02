bda0d37254b381aa35c1a036906f9f273f6ef83c
Rich FitzJohn <rich.fitzjohn@gmail.com>
2013-12-02 17:15:13 +1100

    More notes on building schedules

--- scripts/build_schedule.R
+++ scripts/build_schedule.R
@@ -4,8 +4,6 @@
 ## TODO: Think about if we converge to the same place with both
 ## approaches!
 
-## TODO: Compare with Daniel's approach?
-
 ## TODO: Cohort merging needs implementing (?)
 library(tree)
 library(parallel)
@@ -29,7 +27,7 @@ times.default <- build.schedule(p, 20, t.default, 1e-3,
 
 ## Next, look at what an "optimal" schedule would look like from the
 ## point of only fitness looks like.  We'll aim for a schedule of 250
-## total points:
+## total points (this is about 32 hours work, embarassingly enough).
 n.total <- 250
 n.linear <- n.total - length(t.linear)
 n.default <- n.total - length(t.default)
@@ -43,3 +41,12 @@ times.w.default <-
                                     progress=TRUE,
                                     verbose=TRUE),
              "times.w.default.rds")
+
+## These look like they're grinding away and constantly increasing
+## fitness.  I wonder if this is because of errors propagating up from
+## lower level calculations?
+w.linear  <- sapply(attr(times.w.linear,  "progress")[-1], "[[", "w")
+w.default <- sapply(attr(times.w.default, "progress")[-1], "[[", "w")
+
+plot(w.default, type="l")
+lines(w.linear[seq(to=length(w.linear), length=length(w.default))])
