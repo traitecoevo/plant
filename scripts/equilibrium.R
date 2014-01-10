bf3453e62461578da83a94649b9a5be7a393fd8f
Rich FitzJohn <rich.fitzjohn@gmail.com>
2014-01-10 11:08:51 +1100

    Identify how much noise is coming from the adaptive integration
    (spoiler alert: lots.  See also #53)

--- scripts/equilibrium.R
+++ scripts/equilibrium.R
@@ -10,6 +10,9 @@ p$seed_rain <- 1.1                       # Starting rain.
 p$set_parameters(list(patch_area=1.0))   # See issue #13
 p$set_control_parameters(fast.control()) # A bit faster
 
+## TODO: Still have the issue that the times created here don't appear
+##   to reach 104 (that might actually account for some of the
+##   difference with the reference model?)
 times <- build.schedule(p, 20, cohort.introduction.times(104), 1e-3,
                         progress=TRUE, verbose=TRUE)
 res <- list(seeds.in  = p$seed_rain,
@@ -17,6 +20,10 @@ res <- list(seeds.in  = p$seed_rain,
             times     = as.numeric(times))
 
 history <- list(res)
+
+approach <- t(sapply(history, function(x)
+                     x[c("seeds.in", "seeds.out")]))
+colnames(approach) <- c("in", "out")
 
 ## Once we have the cohort merging working, we could use the previous
 ## iteration's schedule here, I think.  Plus do a savage cull before
@@ -38,11 +45,6 @@ for (i in seq_len(5)) {
 
 ## Then, in the vinicity of the root we should look at what the curve
 ## actually looks like, without adaptive refinement.
-diff <- sapply(history, function(x) x$seeds.out - x$seeds.in)
-i <- which.min(abs(diff))
-w.hat <- history[[i]]$seeds.in
-times <- history[[i]]$times
-
 f <- function(w, p, times) {
   p$seed_rain <- w
   ebt <- new(EBT, p)
@@ -51,57 +53,101 @@ f <- function(w, p, times) {
   ebt$fitness(1)
 }
 
-dw <- 2
 w.hat <- last(history)[["seeds.out"]]
+times <- last(history)[["times"]]
+dw <- 2 # range of input to vary (plus and minus this many seeds)
+
 seeds.in <- seq(w.hat - dw, w.hat + dw, length=31)
 seeds.out <- unlist(mclapply(seeds.in, f, p, times))
 
-## This looks way better than before; that's really great.
+fit <- lm(seeds.out ~ seeds.in)
+
+## Here is input seeds vs output seeds; this function *should* be
+## smooth.
 plot(seeds.in, seeds.out, xlab="Incoming seed rain",
      ylab="Outgoing seed rain", las=1)
 abline(0, 1)
-abline(lm(seeds.out ~ seeds.in), lty=2)
+abline(fit, lty=2)
+
+## TODO: This shows that we're off by a bit because of not including the
+## last time.  Gah.  Need to go through and sort times out soon.
+##
+##   points(approach)
+##
+## TODO: Once that's worked out, add the approach points on here.
 
 ########################################
 
-## Rerun, using a more fine-scale error control, especially around the
-## assimilation calculation.
-ctrl.fine <- fast.control()
-ctrl.fine$plant_assimilation_tol <- 1e-6
-ctrl.fine$ode_tol_rel <- 1e-6
-ctrl.fine$ode_tol_abs <- 1e-6
-
-p.fine <- p$clone()
-p.fine$set_control_parameters(ctrl)
+## Rerun, but allowing adaptive integration during the calculation:
+p.adaptive <- p$clone()
+p.adaptive$set_control_parameters(list(plant_assimilation_adaptive=TRUE))
 
 ## This runs quite a bit slower:
-system.time(f(w.hat, p,      times)) #  9.1s
-system.time(f(w.hat, p.fine, times)) # 46.0s
+system.time(f(w.hat, p,          times)) #  9.1s
+system.time(f(w.hat, p.adaptive, times)) # 18.5s
 
-## Run over the whole range of incoming seeds (this is very slow!)
-seeds.out.fine <- unlist(mclapply(seeds.in, f, p.fine, times))
+seeds.out.adaptive <- unlist(mclapply(seeds.in, f, p.adaptive, times))
+fit.adaptive <- lm(seeds.out.adaptive ~ seeds.in)
 
 matplot(cbind(seeds.in,  seeds.in),
-        cbind(seeds.out, seeds.out.fine),
+        cbind(seeds.out, seeds.out.adaptive),
         xlab="Incoming seed rain", ylab="Outgoing seed rain",
         las=1, pch=1, col=c("black", "red"))
 abline(0, 1)
-abline(lm(seeds.out      ~ seeds.in), lty=2)
-abline(lm(seeds.out.fine ~ seeds.in), lty=2, col="red")
+abline(fit,          lty=2)
+abline(fit.adaptive, lty=2, col="red")
 
 ## This might be easier to see in terms of residuals:
-fit1 <- lm(seeds.out      ~ seeds.in)
-fit2 <- lm(seeds.out.fine ~ seeds.in)
-matplot(seeds.in, cbind(resid(fit1), resid(fit2)),
+
+matplot(seeds.in,
+        cbind(resid(fit), resid(fit.adaptive)),
         xlab="Incoming seed rain", ylab="Residual seed rain",
         las=1, pch=1, col=c("black", "red"))
 abline(h=0, v=w.hat)
 
 ########################################
 
+## Rerun, using a more fine-scale error control, especially around the
+## assimilation calculation, but keeping the non-adaptive
+## integration.
+p.fine <- p$clone()
+p.fine$set_control_parameters(list(plant_assimilation_tol=1e-6,
+                                   ode_tol_rel=1e-6,
+                                   ode_tol_abs=1e-6))
+
+## This runs quite a bit slower:
+system.time(f(w.hat, p,      times)) #  9.1s
+system.time(f(w.hat, p.fine, times)) # 46.0s
+
+seeds.out.fine <- unlist(mclapply(seeds.in, f, p.fine, times))
+
+matplot(seeds.in,
+        cbind(seeds.out, seeds.out.adaptive, seeds.out.fine),
+        xlab="Incoming seed rain", ylab="Outgoing seed rain",
+        las=1, pch=1, col=c("black", "red", "blue"))
+abline(0, 1)
+abline(fit,          lty=2)
+abline(fit.adaptive, lty=2, col="red")
+abline(fit.fine,     lty=2, col="blue")
+
+fit.fine     <- lm(seeds.out.fine     ~ seeds.in)
+matplot(seeds.in,
+        cbind(resid(fit), resid(fit.adaptive), resid(fit.fine)),
+        xlab="Incoming seed rain", ylab="Residual seed rain",
+        las=1, pch=1, col=c("black", "red", "blue"))
+abline(h=0, v=w.hat)
+
+########################################
+
+## TODO: Rerun, but allowing the cohort schedule to be built during
+## the approach.
+
+########################################
+
 ## Compare with falster-traitdiversity.  This is a bit more
-## complicated because teh cohort refinement is going on as well
-## (whereas above we are using the same spacings).
+## complicated because the cohort refinement is going on as well
+## (whereas above we are using the same spacings).  So we don't really
+## have comparable levels of "noise".
 if (!evolve.is.installed())
   install.evolve()
 
@@ -139,19 +185,27 @@ f.ref <- function(w, p) {
 seeds.in.ref <- seq(w.hat.ref - dw, w.hat.ref + dw, length=31)
 seeds.out.ref <- sapply(seeds.in.ref, f.ref, p)
 
+fit.ref <- lm(seeds.out.ref ~ seeds.in.ref)
+
 plot(seeds.in.ref, seeds.out.ref)
 abline(0, 1)
-abline(lm(seeds.out.ref ~ seeds.in.ref), lty=2)
+abline(fit.ref, lty=2)
 
 ## Compared against tree:
-matplot(cbind(seeds.in,  seeds.in,       seeds.in.ref),
-        cbind(seeds.out, seeds.out.fine, seeds.out.ref),
+matplot(cbind(seeds.in,  seeds.in,           seeds.in.ref),
+        cbind(seeds.out, seeds.out.adaptive, seeds.out.ref),
         xlab="Incoming seed rain", ylab="Outgoing seed rain",
-        las=1, pch=1, col=c("black", "red", "blue"))
+        las=1, pch=1, col=c("black", "red", "green4"))
 abline(0, 1)
-abline(lm(seeds.out      ~ seeds.in),     lty=2)
-abline(lm(seeds.out.fine ~ seeds.in),     lty=2, col="red")
-abline(lm(seeds.out.ref  ~ seeds.in.ref), lty=2, col="blue")
+abline(fit,          lty=2)
+abline(fit.adaptive, lty=2, col="red")
+abline(fit.ref,      lty=2, col="green4")
+
+matplot(cbind(seeds.in,   seeds.in,            seeds.in.ref),
+        cbind(resid(fit), resid(fit.adaptive), resid(fit.ref)),
+        xlab="Incoming seed rain", ylab="Residual seed rain",
+        las=1, pch=1, col=c("black", "red", "green4"))
+abline(h=0, v=w.hat)
 
 ########################################
 
@@ -167,13 +221,17 @@ matplot(seeds.in.wide, cbind(seeds.out.wide, seeds.out.wide.ref),
         las=1, type="l", col=c("black", "blue"), lty=1,
         xlab="Incoming seed rain", ylab="Outgoing seed rain")
 abline(0, 1, col="darkgrey")
-abline(lm(seeds.out      ~ seeds.in),     lty=2)
-abline(lm(seeds.out.fine ~ seeds.in),     lty=2, col="red")
-abline(lm(seeds.out.ref  ~ seeds.in.ref), lty=2, col="blue")
+abline(fit,          lty=2)
+abline(fit.adaptive, lty=2, col="red")
+abline(fit.ref,      lty=2, col="green4")
 
-## TODO: Confirm that the instability is due to the adaptive
-## integration.  If so we need to be careful about how that is used in
-## general.
+matplot(seeds.in.wide, cbind(seeds.out.wide, seeds.out.wide.ref),
+        las=1, type="l", col=c("black", "blue"), lty=1, asp=1,
+        xlab="Incoming seed rain", ylab="Outgoing seed rain")
+abline(0, 1, col="darkgrey")
+abline(fit,          lty=2)
+abline(fit.adaptive, lty=2, col="red")
+abline(fit.ref,      lty=2, col="green4")
 
 ## TODO: Review the list of control parameters and see which are
 ## causing the problems.
