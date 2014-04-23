a609007ff9703168e1aa92f5fb27a46de02dd0c8
Felix Lawrence <felix.lawrence@nicta.com.au>
2014-04-23 11:55:52 +1000

    Add Python (heresy!) scripts to gen landscapes
    Added the scripts I have used to bulk-generate fitness landscapes for
    the one- and part of the two-species case (not varying disturbance time
    etc). The one species script runs overnight on my machine, the others
    took 18 hours.
    
    You'll need numpy and rpy2 to run these (unless you want to trivially
    port them to R), and the plotting code commented out at the bottom was
    written for IPython started with the --pylab option (which I think does
    from matplotlib.pylab import *).

---
+++ scripts/one_species_fitnesses.py
@@ -0,0 +1,96 @@
+import rpy2.robjects as robjects
+import rpy2.robjects.numpy2ri
+rpy2.robjects.numpy2ri.activate()
+import numpy as np
+
+# Load tree in R
+robjects.r('source("~/.Rprofile")') # hopefully this sets .libPaths
+robjects.r("library(tree)")
+
+def run_simmo(pop_lma_arr, seed_rain_arr = None):
+    """ Run tree to calculate a fitness landscape with given species.
+
+        Based on landscape2.R.
+    """
+    robjects.r("p <- new(Parameters)")
+
+    # Starting seed rain
+    if seed_rain_arr == None:
+        seed_rain_arr = 1.1 * np.ones_like(pop_lma_arr)
+
+    assert len(pop_lma_arr) == len(seed_rain_arr)
+
+    for lma in pop_lma_arr:
+        # Add species into simulation
+        robjects.r("p$add_strategy(new(Strategy, list(lma=%f)))" % lma)
+
+    robjects.r("p$seed_rain <- c(%s)" % str(list(seed_rain_arr))[1:-1])
+
+    robjects.r("""
+        p$set_parameters(list(patch_area=1.0))   # See issue #13
+        p$set_control_parameters(fast.control()) # A bit faster
+
+        t.max <- p$disturbance$cdf(tree:::reference.pr.survival.eps)
+        times0 <- cohort.introduction.times(t.max)
+        """)
+    robjects.r("schedule0 <- schedule.from.times(times0, %iL)" % len(pop_lma_arr))
+    robjects.r("""
+        res <- equilibrium.seed.rain(p, schedule0, 10,
+                                     build.args=list(verbose=TRUE),
+                                     progress=TRUE, verbose=TRUE)
+        # Set the seed rain back into the parameters
+        p$seed_rain <- res$seed_rain[,"out"]
+
+        # And rerun again to get the ODE times.
+        schedule <- res$schedule
+        ebt <- run.ebt(p, schedule$copy())
+        schedule$ode_times <- ebt$ode_times
+
+        # Mutant LMA values, in increasing numbers, to test how the time
+        # requirements scale with the number of strategies.  Should be
+        # sublinear.
+        lma.f <- seq_log(0.03, 0.8, 64)
+
+        w.f <- landscape(lma.f, p, schedule)
+        """)
+
+    out_seed_rain = np.array(robjects.r('res$seed_rain[,"out"]'))
+    fitness = np.array(robjects.r['w.f'])
+    lma = np.array(robjects.r['lma.f'])
+    return out_seed_rain, fitness, lma
+
+
+seed_rains = {}
+fitnesses = {}
+
+last_seed_rain = [217]
+
+for lma in np.logspace(np.log(0.03), np.log(0.8), 64, base=np.e)[5:-5]:
+    last_seed_rain, fitness, _ = run_simmo([lma], last_seed_rain)
+    seed_rains[lma] = last_seed_rain[0]
+    fitnesses[lma] = fitness
+
+
+## Assorted sloppy bits that I type into IPython --pylab to run this:
+
+# %run /path/to/this/script.py
+
+## Save raw results to avoid recomputation (nb hard-coded filenames)
+# import pickle
+# with open("~/Data/output/tree/one_species_lmas.pickle", 'w') as f:
+#     pickle.dump(np.array(robjects.r['lma.f']), f)
+# with open("~/Data/output/tree/one_species_fitnesses.pickle", 'w') as f:
+#     pickle.dump(fitnesses, f)
+# with open("~/Data/output/tree/one_species_seed_rains.pickle", 'w') as f:
+#     pickle.dump(seed_rains, f)
+
+# # Prepare results and plot
+# all_fit = np.logspace(np.log(0.03), np.log(0.8), 64, base=np.e)
+# fs = np.zeros((len(fitnesses), len(fitnesses.values()[0])))
+# for i, k in enumerate(sort(fitnesses.keys())):
+#     fs[i] = fitnesses[k]
+# figure()
+# loglog()
+# plot(all_fit[5:-5], all_fit[5:-5],'w')
+# imshow(fs/np.mean(fs, axis=1, keepdims=True),
+#        extent=(0.03, 0.8, max(fitnesses.keys()), min(fitnesses.keys())))
