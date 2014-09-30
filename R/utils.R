f86dd1853a306cfcb65ba75cfaeb1d79edb7c8a8
Rich FitzJohn <rich.fitzjohn@gmail.com>
2014-09-30 13:27:58 +1000

    Wrapper function around the nonlinear solvers
    Fixes #101

--- R/utils.R
+++ R/utils.R
@@ -128,3 +128,69 @@ recycle_simple <- function(x, n) {
   }
   x
 }
+
+##' Thin wrapper around \code{nleqslv} and \code{dfsane}
+##' @title Thin wrapper around nleqslv and dfsane
+##' @param x Starting point
+##' @param fn Function to solve
+##' @param tol Tolerance (for \code{nleqslv} this will be both
+##' absolute and relative)
+##' @param maxit Maximum number of iterations.  The number of function
+##' evaluations will likely exceed this.
+##' @param solver The solver to use.  Either "nleqslv" or "dfsane" for now.
+##' @export
+nlsolve <- function(x, fn, tol=1e-6, maxit=100, solver="nleqslv") {
+  solver <- match.arg(solver, c("nleqslv", "dfsane"))
+
+  if (solver == "nleqslv") {
+    res <- nlsolve_nleqslv(x, fn, tol, maxit)
+  } else if (solver == "dfsane") {
+    res <- nlsolve_dfsane(x, fn, tol, maxit)
+  } else {
+    stop("Unknown solver ", solver)
+  }
+
+  if (attr(res, "failed")) {
+    warning(sprintf("Solver has likely failed: code=%d, msg: %s",
+                    sol$termcd, sol$message), immediate.=TRUE)
+  }
+
+  res
+}
+
+nlsolve_nleqslv <- function(x, fn, tol=1e-6, maxit=100) {
+  control <- list(xtol=tol, ftol=tol, maxit=maxit)
+  sol <- nleqslv::nleqslv(x, fn, global="none", control=control)
+  code <- sol$termcd
+  res <- sol$x
+  attributes(res) <- nlsolve_nleqslv_attr(sol)
+  res
+}
+
+nlsolve_nleqslv_attr <- function(sol) {
+  list(y=sol$fvec, # different to dfsane
+       iter=sol$iter,
+       feval=sol$nfcnt, # does not include jacobian evals
+       code=sol$termcd,
+       message=sol$message,
+       failed=sol$termcd > 2 || sol$termcd < 0,
+       solver="nleqslv")
+}
+
+nlsolve_dfsane <- function(x, fn, tol=1e-6, maxit=100) {
+  control <- list(tol=tol, maxit=maxit, trace=FALSE)
+  sol <- BB::dfsane(x, fn, control=control, quiet=TRUE)
+  res <- sol$par
+  attributes(res) <- nlsolve_dfsane_attr(sol)
+  res
+}
+
+nlsolve_dfsane_attr <- function(sol) {
+  list(y=sol$residual,
+       iter=sol$iter,
+       feval=sol$feval,
+       code=sol$convergence,
+       message=sol$message,
+       failed=sol$convergence != 0,
+       solver="dfsane")
+}
