07fbfb27be9036f40c7f592f212d637e35d16a63
Rich FitzJohn <rich.fitzjohn@gmail.com>
2015-02-04 15:20:01 +1100

    Bootstrapped script build system, based on maker

---
+++ scripts/bootstrap.R
@@ -0,0 +1,34 @@
+#!/usr/bin/Rscript
+library(whisker)
+update_file <- function(str, filename) {
+  if (!identical(readLines(filename), str)) {
+    writeLines(str, filename)
+  }
+}
+drop_blank <- function(x) {
+  x <- gsub("\n[[:space:]]*\n", "\n", x)
+  gsub("(^\n|\n$)", "", x)
+}
+
+template <- '
+targets:
+  maker_scripts.yml:
+    depends: "./.scripts"
+    command: system("./bootstrap.R")
+  scripts:
+    depends:
+      - maker_scripts.yml
+{{#scripts}}
+      - {{script}}.md
+{{/scripts}}
+{{#scripts}}
+  {{script}}.Rmd:
+    command: sowsear("{{script}}.R", I("Rmd"))
+  {{script}}.md:
+    knitr: true
+{{/scripts}}'
+
+scripts <- sub("\\.R$", "", readLines(".scripts"))
+vals <- list(scripts=iteratelist(scripts, value="script"))
+yml <- drop_blank(whisker.render(template, vals))
+update_file(yml, "maker_scripts.yml")
