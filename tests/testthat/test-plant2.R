4462b162ea60ae9120ee81d864442be137fcc95f
Daniel Falster <daniel.falster@mq.edu.au>
2015-02-18 06:11:13 +1100

    Expand testing of plant2

--- tests/testthat/test-plant2.R
+++ tests/testthat/test-plant2.R
@@ -17,7 +17,24 @@ test_that("Reference comparison", {
 
   expect_that(vars2, is_a("Plant2_internals"))
 
+  expect_that(p1$height, is_identical_to(p2$height))
   expect_that(vars2[["height"]], is_identical_to(vars1[["height"]]))
-  ## TODO: @dfalster continue adding tests along here, especially for
-  ## the physiological calculations.
+  expect_that(vars2[["leaf_area"]], is_identical_to(vars1[["leaf_area"]]))
+
+  env <- test_environment(h0)
+  light_env <- attr(env, "light_env") # underlying function
+
+  ## Compute the vital rates and compare them
+  p1$compute_vars_phys(env)
+  p2$compute_vars_phys(env)
+
+  vars1 <- p1$internals
+  vars2 <- p2$internals
+
+  expect_that(vars1[["fecundity_rate"]],
+              equals(vars2[["fecundity_rate"]], tolerance=1e-7))
+  expect_that(vars1[["height_growth_rate"]],
+              equals(vars2[["height_rate"]], tolerance=1e-7))
+  expect_that(vars1[["mortality_rate"]],
+              equals(vars2[["mortality_rate"]], tolerance=1e-7))
 })
