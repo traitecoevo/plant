a006737e402ec0e1e371770c335b601a90b81cb3
Rich FitzJohn <rich.fitzjohn@gmail.com>
2015-02-25 10:30:31 +1100

    Regression test for cohort schedule building

--- tests/testthat/test-build-schedule.R
+++ tests/testthat/test-build-schedule.R
@@ -6,3 +6,14 @@ test_that("Corner case", {
   p <- expand_parameters(trait_matrix(0.1, "lma"), p)
   expect_that(build_schedule(p), throws_error("no residents"))
 })
+
+test_that("Schedule building", {
+  ## This is a really dumb test but it should act as a regression test
+  ## at least.
+  p <- ebt_base_parameters()
+  p$strategies <- list(Strategy())
+  p$seed_rain <- 0.1
+  p <- build_schedule(p)
+  expect_that(length(p$cohort_schedule_times_default), equals(141))
+  expect_that(length(p$cohort_schedule_times[[1]]),    equals(176))
+})
