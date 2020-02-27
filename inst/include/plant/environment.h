88b6f2fe223aa93d72292135505ac4ea999da5ec
Rafael Schouten <rafaelschouten@gmail.com>
2020-02-27 16:08:20 +1100

    add dummy ode methods, and move other fields and methods to Environment

--- inst/include/plant/environment.h
+++ inst/include/plant/environment.h
@@ -15,43 +15,81 @@ namespace plant {
 
 class Environment {
 public:
-  double canopy_openness(double height) const;
+
+  Environment() :
+    time(0.0),
+    disturbance_regime(30),
+    seed_rain({}),
+    seed_rain_index(0),
+    environment_generator(interpolator::AdaptiveInterpolator()) {
+  };
+
   template <typename Function>
   void compute_environment(Function f, double height_max);
-  void set_fixed_environment(double competition_amount, double height_max);
-  void set_fixed_environment(double competition_amount);
   template <typename Function>
   void rescale_environment(Function f, double height_max);
-  double patch_survival() const;
-  double patch_survival_conditional(double time_at_birth) const;
-  void clear();
-  void clear_environment();
 
-  double seed_rain_dt() const;
-  void set_seed_rain_index(size_t x);
+  void set_fixed_environment(double value, double height_max) {
+    std::vector<double> x = {0, height_max/2.0, height_max};
+    std::vector<double> y = {value, value, value};
+    clear_environment();
+    environment_interpolator.init(x, y);
+  }
 
-  // * R interface
-  void r_set_seed_rain_index(util::index x);
+  void set_fixed_environment(double value) {
+    double height_max = 150.0;
+    set_fixed_environment(value, height_max);
+  }
+
 
   // Dummy iterators: do nothing if the environment has no state. 
-  ode::iterator ode_state(ode::iterator it) const {
-    return it;
+  ode::iterator ode_state(ode::iterator it) const { return it; }
+  ode::const_iterator set_ode_state(ode::const_iterator it) { return it; }
+  ode::iterator ode_rates(ode::iterator it) const { return it; }
+  int ode_size() const { return 0; }
+
+  // Computes the probability of survival from 0 to time.
+  double patch_survival() const {
+    return disturbance_regime.pr_survival(time);
   }
-  ode::const_iterator set_ode_state(ode::const_iterator it) {
-    return it;
+  // Computes the probability of survival from time_at_birth to time, by
+  // conditioning survival over [0,time] on survival over
+  // [0,time_at_birth].
+  double patch_survival_conditional(double time_at_birth) const {
+    return disturbance_regime.pr_survival_conditional(time, time_at_birth);
   }
-  ode::iterator ode_rates(ode::iterator it) const {
-    return it;
+  // Reset the environment.
+  void clear() {
+    time = 0.0;
+    clear_environment();
   }
-  int ode_size() const {
-    return 0;
+  void clear_environment() {
+    environment_interpolator.clear();
+  }
+  double seed_rain_dt() const {
+    if (seed_rain.empty()) {
+      Rcpp::stop("Cannot get seed rain for empty environment");
+    }
+    return seed_rain[seed_rain_index];
+  }
+  void set_seed_rain_index(size_t x) {
+    seed_rain_index = x;
+  }
+  // * R interface
+  void r_set_seed_rain_index(util::index x) {
+    set_seed_rain_index(x.check_bounds(seed_rain.size()));
+  }
+
+
+  double get_environment_at_height(double height) const {
+    const bool within = height <= environment_interpolator.max();
+    // TODO: change maximum - here hard-coded to 1.0
+    return within ? environment_interpolator.eval(height) : 1.0;
   }
 
   double time;
   Disturbance disturbance_regime;
   interpolator::Interpolator environment_interpolator;
-
-private:
   std::vector<double> seed_rain;
   size_t seed_rain_index;
   interpolator::AdaptiveInterpolator environment_generator;
--- inst/include/plant/light_environment.h
+++ inst/include/plant/light_environment.h
@@ -21,87 +21,66 @@ public:
     disturbance_regime = 30;
     seed_rain = { 1.0, 1.0, 1.0 };
     seed_rain_index = 3;
+    k_I = 0.5;
     environment_generator = interpolator::AdaptiveInterpolator(1e-6, 1e-6, 17, 16);
     // Define an anonymous function to pass got the environment generator
     environment_interpolator = environment_generator.construct(
       [&](double height) {
-          return FF16_Environment::canopy_openness(height);
-      }, 30, 150.0);
-    k_I = 0.5;
+          return get_environment_at_height(height);
+      }, 0, 1); // these are update with init(x, y) whne patch is created
   };
 
   FF16_Environment(double disturbance_mean_interval,
                    std::vector<double> seed_rain_,
                    double k_I_,
-                   Control control)
-    : time(0.0),
-      disturbance_regime(disturbance_mean_interval),
-      seed_rain(seed_rain_),
-      seed_rain_index(0),
-      k_I(k_I_),
-      environment_generator(interpolator::AdaptiveInterpolator(control.environment_light_tol,
+                   Control control) {
+      k_I = k_I_;
+      environment_generator = interpolator::AdaptiveInterpolator(control.environment_light_tol,
                                 control.environment_light_tol,
                                 control.environment_light_nbase,
-                                control.environment_light_max_depth)) {
+                                control.environment_light_max_depth);
+      time = 0.0;
+      disturbance_regime = disturbance_mean_interval;
+      seed_rain = seed_rain_;
+      seed_rain_index = 0;
   };
 
-  void set_fixed_environment(double competition_amount, double height_max);
-  void set_fixed_environment(double competition_amount);
-  double canopy_openness(double height) const;
+  // TODO: move these to Environment 
   template <typename Function>
-  void compute_environment(Function f_compute_competition, double height_max);
+  void compute_environment(Function f_compute_competition,
+                                              double height_max) {
+    const double lower_bound = 0.0;
+    double upper_bound = height_max;
+
+    auto f_canopy_openness = [&] (double height) -> double {return exp(-k_I * f_compute_competition(height));};
+    environment_interpolator =
+      environment_generator.construct(f_canopy_openness, lower_bound, upper_bound);
+  }
+
   template <typename Function>
-  void rescale_environment(Function f_compute_competition, double height_max);
-  double patch_survival() const;
-  double patch_survival_conditional(double time_at_birth) const;
-  void clear();
-  void clear_environment();
+  void rescale_environment(Function f_compute_competition,
+                                             double height_max) {
+    std::vector<double> h = environment_interpolator.get_x();
+    const double min = environment_interpolator.min(), // 0.0?
+      height_max_old = environment_interpolator.max();
 
-  // NOTE: Interface here will change
-  double seed_rain_dt() const;
-  void set_seed_rain_index(size_t x);
-  // * R interface
-  void r_set_seed_rain_index(util::index x);
+    auto f_canopy_openness = [&] (double height) -> double {return exp(-k_I * f_compute_competition(height));};
+    util::rescale(h.begin(), h.end(), min, height_max_old, min, height_max);
+    h.back() = height_max; // Avoid round-off error.
 
-  double time;
-  Disturbance disturbance_regime;
-  interpolator::Interpolator environment_interpolator;
+    environment_interpolator.clear();
+    for (auto hi : h) {
+      environment_interpolator.add_point(hi, f_canopy_openness(hi));
+    }
+    environment_interpolator.initialise();
+  }
 
-private:
-  std::vector<double> seed_rain;
-  size_t seed_rain_index;
-  interpolator::AdaptiveInterpolator environment_generator;
+  double canopy_openness(double height) const {
+    return get_environment_at_height(height);
+  }
+
   double k_I;
 };
-
-template <typename Function>
-void FF16_Environment::compute_environment(Function f_compute_competition,
-                                            double height_max) {
-  const double lower_bound = 0.0;
-  double upper_bound = height_max;
-
-  auto f_canopy_openness = [&] (double height) -> double {return exp(-k_I * f_compute_competition(height));};
-  environment_interpolator =
-    environment_generator.construct(f_canopy_openness, lower_bound, upper_bound);
-}
-
-template <typename Function>
-void FF16_Environment::rescale_environment(Function f_compute_competition,
-                                           double height_max) {
-  std::vector<double> h = environment_interpolator.get_x();
-  const double min = environment_interpolator.min(), // 0.0?
-    height_max_old = environment_interpolator.max();
-
-  auto f_canopy_openness = [&] (double height) -> double {return exp(-k_I * f_compute_competition(height));};
-  util::rescale(h.begin(), h.end(), min, height_max_old, min, height_max);
-  h.back() = height_max; // Avoid round-off error.
-
-  environment_interpolator.clear();
-  for (auto hi : h) {
-    environment_interpolator.add_point(hi, f_canopy_openness(hi));
-  }
-  environment_interpolator.initialise();
-}
 
 }
 
