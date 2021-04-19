519239302624facf7310ac2b7f6e79f7ca5afbfc
Daniel Falster <daniel.falster@unsw.edu.au>
2021-04-19 22:28:42 +1000

    Re-enable some tests on K93 strategy

--- tests/testthat/test-strategy-k93.R
+++ tests/testthat/test-strategy-k93.R
@@ -70,35 +70,36 @@ test_that("K93_Strategy hyper-parameterisation", {
 })
 
 ## Number of ODE steps is unstable - needs review
-# test_that("K93 seed rain is unchanged", {
+test_that("K93 seed rain is unchanged", {
 
-#  # Generic parameters
-#  p0 <- scm_base_parameters("K93")
-#  p0$disturbance_mean_interval <- 10
+  # Generic parameters
+  p0 <- scm_base_parameters("K93")
+  p0$disturbance_mean_interval <- 10
+  p0$strategy_default$k_I <- 1e-6  
 
-#  # Use single sp. defaults
-#  p1 <- expand_parameters(trait_matrix(0.059, "b_0"), p0, mutant = FALSE)
-#  p1$seed_rain <- 20
+  # Use single sp. defaults
+  p1 <- expand_parameters(trait_matrix(0.059, "b_0"), p0, mutant = FALSE)
+  p1$seed_rain <- 20
 
-#  out <- run_scm(p1)
-#  expect_equal(out$seed_rains, 0.0752, tolerance = 1e-4)
-#  expect_equal(out$ode_times[c(10, 100)], c(0.000070, 4.500004), tolerance = 1e-5)
+  out <- run_scm(p1)
+  expect_equal(out$seed_rains, 0.0752, tolerance = 1e-4)
+  # expect_equal(out$ode_times[c(10, 100)], c(0.000070, 4.500004), tolerance = 1e-5)
 
-#  # Three species from paper
-#  sp <- trait_matrix(c(0.042, 0.063, 0.052,
-#                       8.5e-3, 0.014, 0.015,
-#                       2.2e-4, 4.6e-4, 3e-4,
-#                       0.008, 0.008, 0.008,
-#                       1.8e-4, 4.4e-4, 5.1e-4,
-#                       1.4e-4, 2.5e-3, 8.8e-3,
-#                       0.044, 0.044, 0.044),
-#                     c("b_0", "b_1", "b_2",
-#                       "c_0", "c_1", "d_0", "d_1"))
+  # Three species from paper
+  sp <- trait_matrix(c(0.042, 0.063, 0.052,
+                        8.5e-3, 0.014, 0.015,
+                        2.2e-4, 4.6e-4, 3e-4,
+                        0.008, 0.008, 0.008,
+                        1.8e-4, 4.4e-4, 5.1e-4,
+                        1.4e-4, 2.5e-3, 8.8e-3,
+                        0.044, 0.044, 0.044),
+                      c("b_0", "b_1", "b_2",
+                        "c_0", "c_1", "d_0", "d_1"))
 
-#  p2 <- expand_parameters(sp, p0, mutant = FALSE)
-#  p2$seed_rain <- c(20, 20, 20)
-#  out <- run_scm(p2)
+  p2 <- expand_parameters(sp, p0, mutant = FALSE)
+  p2$seed_rain <- c(20, 20, 20)
+  out <- run_scm(p2)
 
-#  expect_equal(out$seed_rains, c(0.0025, 0.2314, 0.2195), tolerance = 1e-4)
-#  expect_equal(length(out$ode_times), 224)
-#})
+  expect_equal(out$seed_rains, c(0.0025, 0.2314, 0.2195), tolerance = 1e-4)
+  #expect_equal(length(out$ode_times), 224)
+})
