204fb25e171560266124baa4cd1b6fbcc32b3128
aornugent <aornugent@gmail.com>
2021-10-11 12:42:09 +1100

    Add FF16w to benchmarks

--- R/benchmark.R
+++ R/benchmark.R
@@ -8,14 +8,13 @@
 
 run_plant_benchmarks <- function(strategy_types = list(FF16 = FF16_Strategy,
                                                        FF16r = FF16r_Strategy,
+                                                       FF16w = FF16w_Strategy,
                                                        K93 = K93_Strategy),
                                  iterations = 1) {
   f_scm <- function(x) {
     p0 <- scm_base_parameters(x)
-    ctrl <- scm_base_control()
-    p <-
-      expand_parameters(trait_matrix(0.0825, "lma"), p0, mutant = FALSE)
-    res <- run_scm(p, ctrl)
+    p <- expand_parameters(trait_matrix(0.0825, "lma"), p0, mutant = FALSE)
+    res <- run_scm(p)
   }
 
   f_build_schedule <- function(x) {
