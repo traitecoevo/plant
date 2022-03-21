4d8b9261a9cfe1f66fa9cb8e58131e7a5c170e99
Andrew O'Reilly-Nugent <aornugent@gmail.com>
2022-03-21 17:12:39 +1100

    Add headers and QAG integrator

--- src/ff16w_strategy.cpp
+++ src/ff16w_strategy.cpp
@@ -14,6 +14,7 @@ FF16w_Strategy::FF16w_Strategy() {
 double FF16w_Strategy::net_mass_production_dt(const FF16_Environment& environment,
                                 double height, double area_leaf_,
                                 bool reuse_intervals) {
+
   const double mass_leaf_    = mass_leaf(area_leaf_);
   const double area_sapwood_ = area_sapwood(area_leaf_);
   const double mass_sapwood_ = mass_sapwood(area_sapwood_, height);
@@ -22,31 +23,32 @@ double FF16w_Strategy::net_mass_production_dt(const FF16_Environment& environmen
   const double mass_root_    = mass_root(area_leaf_);
   const double assimilation_ = assimilator.assimilate(environment, height,
                                                       area_leaf_, reuse_intervals);
-  const double respiration_ =
-    respiration(mass_leaf_, mass_sapwood_, mass_bark_, mass_root_);
-  const double turnover_ =
-    turnover(mass_leaf_, mass_sapwood_, mass_bark_, mass_root_);
+  const double respiration_ = respiration(mass_leaf_, mass_sapwood_,
+                                          mass_bark_, mass_root_);
 
-water_use(environment, assimilation_);
+  const double turnover_ =  turnover(mass_leaf_, mass_sapwood_,
+                                    mass_bark_, mass_root_);
+
+  // water_use(environment, assimilation_);
   return net_mass_production_dt_A(assimilation_, respiration_, turnover_);
 }
 
 // TODO: basic test of accessing environment state
-void FF16w_Strategy::water_use(const FF16_Environment &environment,
-                                    double assimilation) {
-//calculate water use based on assimilation
-std::vector<double> water = environment.get_soil_water_state();
-//just working with one layer first
-double ca_ = 42 //partial pressure atmospheric CO2 (Pa)
-double ci_ = 29 //partial pressure intercellular CO2 (Pa)
-double p_atm_ = 101325 //atmospheric pressure (Pa)
-
-double g_c_ = assimilation_ / ((ca_ - ci_) / p_atm_); //stomatal conductance to co2 (mol c02 m^2 leaf s^-1)
-double g_s_ = 1.6 *g_s_ //stomatal conductance to h20 (mol h20 m^2 leaf s^-1)
-double water_use_ = g_s_ * (VPD / p_atm_) * LAI // VPD (vapour pressure deficit; PA), LAI leaf area index at given height
-// will use consumption rates once merged
-  environment.set_soil_water_rate(-water_use_);
-}
+// void FF16w_Strategy::water_use(const FF16_Environment &environment,
+//                                     double assimilation) {
+//   //calculate water use based on assimilation
+//   std::vector<double> water = environment.get_soil_water_state();
+//   //just working with one layer first
+//   double ca_ = 42 //partial pressure atmospheric CO2 (Pa)
+//   double ci_ = 29 //partial pressure intercellular CO2 (Pa)
+//   double p_atm_ = 101325 //atmospheric pressure (Pa)
+//
+//   double g_c_ = assimilation_ / ((ca_ - ci_) / p_atm_); //stomatal conductance to co2 (mol c02 m^2 leaf s^-1)
+//   double g_s_ = 1.6 *g_s_ //stomatal conductance to h20 (mol h20 m^2 leaf s^-1)
+//   double water_use_ = g_s_ * (VPD / p_atm_) * LAI // VPD (vapour pressure deficit; PA), LAI leaf area index at given height
+//   // will use consumption rates once merged
+//   environment.set_soil_water_rate(-water_use_);
+// }
 
 FF16w_Strategy::ptr make_strategy_ptr(FF16w_Strategy s) {
   s.prepare_strategy();
--- src/leaf_model.cpp
+++ src/leaf_model.cpp
@@ -1,60 +1,82 @@
 #include <plant/leaf_model.h>
-#include <Rcpp.h>
-#include <plant/util_post_rcpp.h>
 
 namespace plant {
-namespace leaf {
+Leaf::Leaf(){}
 
-void leaf_model::calc_soil_water_pot(const FF16_Environment &environment, double psi_aep, double b_CH) {
-    std::vector<double> rel_theta = environment.get_soil_water_state();
-    return psi_aep*pow(rel_theta, b_CH)
 
-void leaf_model::calc_k_l_max(double K_s, double h_v, double h) const {
+// TODO: vectorise for soil layers
+double Leaf::calc_soil_water_pot(const FF16_Environment &environment, double psi_aep, double b_CH) {
+    double rel_theta = environment.get_soil_water_state()[0]; // this hack assumes one soil layer
+    return psi_aep*pow(rel_theta, b_CH);
+}
+
+double Leaf::calc_k_l_max(double K_s, double h_v, double h) const {
   return K_s*h_v/h;
 }
 
-void leaf_model::calc_vul_b(double p_50, double c) const {
-    return p_50/
-    pow(-log(1-50/100)), 1/c);
+double Leaf::calc_vul_b(double p_50, double c) const {
+    return p_50 / pow(-log(1-50/100), 1/c);
 }
 
-void leaf_model::calc_cond_vuln(double K_s, double h_v, double h, double p_50, double c) const {
-    return calc_k_l_max(K_s, h_v, h)*exp(-(x/calc_vul_b(p_50, c)));
+// integrates
+double Leaf::calc_cond_vuln(double psi, double k_l_max, double b, double c) const {
+    return k_l_max * exp(-pow((psi / b), c));
 }
 
-void leaf_model::calc_g_c(double psi_stem, double psi_soil, double atm_vpd, double psi_aep, double b_CH, const kg_2_mol_h20){
-    return atm_kpa*calc_E_supply(psi_stem, psi_soil = calc_soil_water_pot(FF16_Environment &environment, psi_aep, b_CH), K_s, h_v, h, p_50, c)*kg_2_mol_h20/atm_vpd
-    /1.6;
+// replace f with some other function
+double calc_E_supply(double k_l_max, double b, double c, double psi_soil, double psi_stem){
+    std::function<double(double)> f;
+    f = [&] (double psi) -> double {
+         return calc_cond_vuln(psi, k_l_max, b, c);
+       };
+
+  integrator.integrate(f, psi_soil, psi_stem)
 }
 
-void leaf_model::calc_A_c(double c_i, double vcmax, double gamma_25, double umol_per_mol_2_Pa, double km_25){
+//,
+double Leaf::calc_g_c(double psi_stem, double psi_soil, double atm_kpa, double atm_vpd, double psi_aep, double b_CH, const double kg_2_mol_h2o){
+    double = psi_soil = calc_soil_water_pot(FF16_Environment &environment, psi_aep, b_CH);
+    double E_supply = calc_E_supply(psi_stem, psi_soil, k_l_max, p_50, c, b);
+
+    return atm_kpa * E_supply * kg_2_mol_h2o / atm_vpd / 1.6;
+}
+
+double Leaf::calc_A_c(double c_i, double vcmax, double gamma_25, double umol_per_mol_2_Pa, double km_25){
     return (vcmax * (c_i - gamma_25*umol_per_mol_2_Pa))/(c_i + km_25);
 }
 
-void leaf_model::calc_A_j(double PPFD, double vcmax, double vcmax_25_2_jmax_25, double curv_fact, double a, double gamma_25, double umol_per_mol_2_Pa, double c_i){
-    jmax = vcmax*vcmax_25_2_jmax_25
-    j = (a*PPFD + jmax - sqrt((a*PPFD+jmax)^2-4*curv_fact*PPFD*jmax))/(2*curv_fact)
-    return j/4 * ((c_i - gamma_25*umol_per_mol_2_Pa)/(c_i + 2*gamma_25*umol_per_mol_2_Pa));
+double Leaf::calc_A_j(double PPFD, double vcmax, double vcmax_25_2_jmax_25, double curv_fact, double a, double gamma_25, double umol_per_mol_2_Pa, double c_i){
+    double jmax = vcmax*vcmax_25_2_jmax_25;
+    double j = (a*PPFD + jmax - sqrt((a*PPFD+jmax)^2-4*curv_fact*PPFD*jmax))/(2*curv_fact);
+    return j / 4 * ((c_i - gamma_25*umol_per_mol_2_Pa)/(c_i + 2*gamma_25*umol_per_mol_2_Pa));
 }
 
-void leaf_model::calc_A_lim(double PPFD, double vcmax, double vcmax_25_2_jmax_25, double curv_fact, double a, double gamma_25, double umol_per_mol_2_Pa, double c_i){
-     double A_j = calc_A_j(PPFD, vcmax, vcmax_25_2_jmax_25, curv_fact, a, gamma_25, umol_per_mol_2_Pa, c_i)
-     double A_c = calc_A_c(PPFD, vcmax, vcmax_25_2_jmax_25, curv_fact, a, gamma_25, umol_per_mol_2_Pa, c_i)
+double Leaf::calc_A_lim(double PPFD, double vcmax, double vcmax_25_2_jmax_25, double curv_fact, double a, double gamma_25, double umol_per_mol_2_Pa, double c_i){
+     double A_j = calc_A_j(PPFD, vcmax, vcmax_25_2_jmax_25, curv_fact, a, gamma_25, umol_per_mol_2_Pa, c_i);
+     double A_c = calc_A_c(PPFD, vcmax, vcmax_25_2_jmax_25, curv_fact, a, gamma_25, umol_per_mol_2_Pa, c_i);
 
-     return (A_c + A_j  - sqrt((A_c + A_j)^2-4*0.98*A_c*A_j))/(2*0.98);
+     return (A_c + A_j  - sqrt(pow(A_c + A_j, 2) - 4 * 0.98 * A_c * A_j)/(2 * 0.98));
 }
 
-void leaf_model::diff_ci(double PPFD, double vcmax, double vcmax_25_2_jmax_25, double curv_fact, double a, double gamma_25, double umol_per_mol_2_Pa, double c_i){
-     return calc_A_lim(PPFD, vcmax, vcmax_25_2_jmax_25, curv_fact, a, gamma_25, umol_per_mol_2_Pa, c_i)*umol_per_mol_2_mol_per_mol - (calc_g_c(psi_stem, psi_soil, atm_vpd, psi_aep, b_CH, kg_2_mol_h20)*(ca - x)/(atm_kpa*kPa_2_Pa));
+double Leaf::diff_ci(double PPFD, double vcmax, double vcmax_25_2_jmax_25, double curv_fact, double a, double gamma_25, double umol_per_mol_2_Pa, double c_i,
+                     double psi_stem, double psi_soil, double atm_vpd, double psi_aep, double b_CH, double kg_2_mol_h2o, double ca, double x, double atm_kpa, double kPa_2_Pa ){
+     double A_lim_ = calc_A_lim(PPFD, vcmax, vcmax_25_2_jmax_25, curv_fact, a, gamma_25, umol_per_mol_2_Pa, c_i);
+     double g_c_ = calc_g_c(psi_stem, psi_soil, atm_vpd, psi_aep, b_CH, kg_2_mol_h2o)*(ca - x)/(atm_kpa*kPa_2_Pa)
+
+     return A_lim_ * umol_per_mol_2_mol_per_mol - (g_c_ * (ca - x) / (atm_kpa * kPa_2_Pa));
+
+      // calc_A_lim(PPFD, vcmax, vcmax_25_2_jmax_25, curv_fact, a, gamma_25, umol_per_mol_2_Pa, c_i) *umol_per_mol_2_mol_per_mol - (calc_g_c(psi_stem, psi_soil, atm_vpd, psi_aep, b_CH, kg_2_mol_h2o)*(ca - x)/(atm_kpa*kPa_2_Pa));
 }
 
 // need to fill in tol and max_iteratiosn
-void leaf_model::solve_A_lim(double PPFD, double vcmax, double vcmax_25_2_jmax_25, double curv_fact, double a, double gamma_25, double umol_per_mol_2_Pa, double c_i){
-     double ci_root_ = util::uniroot(diff_ci, 0, 40, tol, max_iterations);
-     return calc_A_lim(PPFD, vcmax, vcmax_25_2_jmax_25, curv_fact, a, gamma_25, umol_per_mol_2_Pa, double c_i = ci_root_)
+double Leaf::solve_A_lim(double PPFD, double vcmax, double vcmax_25_2_jmax_25, double curv_fact, double a, double gamma_25, double umol_per_mol_2_Pa, double c_i){
+     double diff_ci_ = diff_ci(PPFD, vcmax, vcmax_25_2_jmax_25, curv_fact, a, gamma_25, umol_per_mol_2_Pa, c_i);
+     double ci_root_ = util::uniroot(diff_ci, 0, 40, 1e-8, 1000); // tol and iterations copied from control defaults (for now)
+     return calc_A_lim(PPFD, vcmax, vcmax_25_2_jmax_25, curv_fact, a, gamma_25, umol_per_mol_2_Pa, double c_i = ci_root_);
 }
 
-void leaf_model::solve_A_lim(double PPFD, double vcmax, double vcmax_25_2_jmax_25, double curv_fact, double a, double gamma_25, double umol_per_mol_2_Pa, double c_i){
-     double ci_root_ = util::uniroot(diff_ci, 0, 40, tol, max_iterations);
-     return calc_A_lim(PPFD, vcmax, vcmax_25_2_jmax_25, curv_fact, a, gamma_25, umol_per_mol_2_Pa, double c_i = ci_root_)
+// void Leaf::solve_A_lim(double PPFD, double vcmax, double vcmax_25_2_jmax_25, double curv_fact, double a, double gamma_25, double umol_per_mol_2_Pa, double c_i){
+//      double ci_root_ = util::uniroot(diff_ci, 0, 40, tol, max_iterations);
+//      return calc_A_lim(PPFD, vcmax, vcmax_25_2_jmax_25, curv_fact, a, gamma_25, umol_per_mol_2_Pa, double c_i = ci_root_);
+// }
 }
