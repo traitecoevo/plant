3369897e4b436097618ca699c637623a66fe86f5
Andrew O'Reilly-Nugent <aornugent@gmail.com>
2022-05-05 19:09:16 +1000

    Refactor for >1D using laGP

--- scripts/optimise-bayesopt.R
+++ scripts/optimise-bayesopt.R
@@ -1,7 +1,6 @@
 library(GPfit)
 
 # Source: https://github.com/bearloga/bayesopt-tutorial-r/blob/master/BayesOpt.R
-
 bayesian_optimize <- function(fn, 
                               evals = NULL,
                               iter = 1,
@@ -24,10 +23,10 @@ bayesian_optimize <- function(fn,
     
     evals$y <- map_dbl(evals$x, calibrate)
   }
- 
+  
   # TODO: GPfit wants x-scale on (0, 1]
   x_new = seq(bounds[1], bounds[2], len = n_eval_pts)
-
+  
   for(i in 0:iter) {
     
     # current best eval
@@ -59,7 +58,7 @@ bayesian_optimize <- function(fn,
     
     # fit/re-fit emulator
     fit <- GP_fit(
-      X = evals$x,
+      x = evals$x,
       Y = evals$y,
       corr = control$cov
     )
@@ -88,6 +87,275 @@ bayesian_optimize <- function(fn,
 }
 
 
+# source: https://bookdown.org/rbg/surrogates/chap7.html
+library(laGP)
+library(lhs)
+
+eps <- sqrt(.Machine$double.eps) ## used lots below
+
+# expected improvement of sampling x compared to the currently observed minima
+EI <- function(gp, x, fmin) {
+  if(is.null(nrow(x))) 
+    x <- matrix(x, nrow=1)
+  
+  p <- predGPsep(gp, x, lite=TRUE)
+  
+  d <- fmin - p$mean
+  sigma <- sqrt(p$s2)
+  dn <- d / sigma
+  ei <- d * pnorm(dn) + sigma * dnorm(dn)
+
+  return(ei)
+}
+
+
+EI.search <- function(x, y, gp, bounds, multi_start = 5, tol = eps) {
+  
+  # find minima of data and start there
+  m <- which.min(y)
+  fmin <- y[m]
+  start <- matrix(x[m, ], nrow=1)
+  
+  # randomly sample around starting point
+  if(multi_start > 1) {
+    start <- rbind(start, randomLHS(multi_start - 1, ncol(x)))
+  } else {
+    message("Using a multi-start optimisation routine
+            can improve numerical instability")
+  }
+  
+  max_ei = 0
+  
+  # repeatedly optimise
+  for(i in 1:nrow(start)) {
+    
+    if(EI(gp, start[i, ], fmin) <= tol) {
+      out <- list(value = -Inf)
+      next 
+    }
+    
+    # optim likes to minimise things, in this case the negative expected 
+    # improvement of the next acquisition to improve the predicted minima of GP
+    objective <- function(x, fmin, gp) {
+      - EI(gp, x, fmin)
+    }
+    
+    limits = tibble(bounds) %>% unnest_wider(bounds)
+    
+    out <- optim(start[i, ], objective, method = "L-BFGS-B", 
+                 lower = limits$min,
+                 upper = limits$max,
+                 gp = gp, fmin = fmin)
+    
+    ei = -out$value
+
+    # save best acquisition location
+    if(ei > max_ei) {
+      max_ei = ei
+      x_next = matrix(out$par, nrow = 1)
+    } 
+  }
+
+  return(x_next)
+}
+
+# @param bounds a list with upper and lower limits for each variable
+generate_seeds <- function(f, bounds, n = 1,
+                           target = NULL,
+                           parameters = NULL,
+                           evaluate = T) {
+  x <- randomLHS(n, length(bounds))
+  
+  for(i in 1:length(bounds)) {
+    x[, i] = bounds[[i]]$min + x[, i] * (bounds[[i]]$max - bounds[[i]]$min)
+  }
+  
+  colnames(x) <- names(bounds)
+  
+  if(evaluate == T) {
+    if(is.null(target) | is.null(parameters)) {
+      stop("Target and parameters must be provided to evaluate seeds")
+    }
+    
+    y <- apply(x, 1, f, target = target, parameters = parameters)
+    return(list(x = x, y = y))
+    
+  } else {
+    return(x)
+  }
+}
+
+
+generate_range <- function(bounds, n = 1000) {
+  x = matrix(nrow = n, ncol= length(bounds))
+  basis = seq(0, 1, len = n)
+  
+  for(i in 1:length(bounds)) {
+    x[, i] = bounds[[i]]$min + basis * (bounds[[i]]$max - bounds[[i]]$min)
+  }
+  
+  return(x)
+}
+
+
+# @param x optimisable parameters
+# @param target a function or vector of points to calculate the marginal likelihood 
+# @param latitude used to specify site characteristics
+# @param n_control_points number of points to evaluate LL if target is a function
+calibrate <- function(x, target, latitude = 28.182,
+                      parameters, 
+                      environment = NULL,
+                      n_control_pts = 10,
+                      match_tissue = "mass_heartwood") {
+  
+  if(is.null(parameters$cohort_schedule_times)) {
+    stop("Please run `create_schedule` first")
+  }
+  
+  # assume optimising B_lf1 and hmat
+  with(as.list(x), {
+    site <- create_site(B_lf1 = B_lf1, latitude = latitude)
+    
+    sp <- create_species(hmat = hmat, site = site)
+    # sp <- create_species(site = site)
+    
+    # overwrite species
+    parameters$strategies <- sp$strategies
+    parameters$birth_rate <- sp$birth_rate
+    
+    if(is.null(environment)) {
+      environment = create_environment()
+    }
+    
+    biomass = calculate_net_biomass(parameters, environment) 
+    
+    if(is.function(target)) {
+
+      # drop final timepoint
+      s = parameters$cohort_schedule_times[[1]]
+      s = s[-length(s)]
+
+      if(n_control_pts == 1) {
+         # target endpoint
+         pts <- max(s)
+      } else { 
+        # generate equidistant control points - not actually n :/
+        pts <- s[findInterval(seq(0, max(s), len = n_control_pts), s)] %>%
+          unique()
+      }
+      
+      if(match_tissue == "total") {
+        biomass <- group_by(biomass, time, species) %>%
+          summarise(tissue = "total",
+                    value = sum(value), 
+                    .groups = "drop")
+      }
+      
+      totals <- filter(biomass, 
+                       tissue == match_tissue, 
+                       time %in% pts) %>%
+        mutate(target = target(time)) %>%
+        mutate(error = target - value)
+    } else {
+      stop("Calibration for dataframes not yet implemented")
+    }
+    
+    # loss fn - emphasise fit of total biomass by downweighting early ctrl pts
+    weights = seq(0.1, 1, len = n_distinct(totals$time))
+    error <- matrix(log(sum(abs(totals$error) * weights)), ncol = 1)
+    
+    return(error)
+  })
+}
 
 
 
+optim.EI <- function(f, target, bounds, parameters, n_iter = 1, n_init = 6,
+                     gp = NULL, evals = NULL) {
+
+  if(is.null(bounds))
+    stop("Please specify the bounds of optimisable parameters")
+    
+  if(is.null(evals)) {
+    if(n_init < 1)
+      stop("Provide a set of initial simulations (seed) or 
+           randomly generate n_init new simulations")
+    
+    evals <- generate_seeds(f, n_init, target, bounds, parameters)
+  }
+  
+  x <- evals$x
+  y <- evals$y
+  
+  # initialise GP in C, with separable correlation for >1 dimensions
+  # d = length scale, g = nugget, dK = save derivative for optimisation of d
+  if(is.null(gp)) {
+    gp_ptr <- newGPsep(x, y, d=0.1, g=1e-6, dK=TRUE)
+    
+    # priors for GP lengthscale, optimised using MLE
+    da <- darg(list(mle = T), 
+               generate_seeds(calibrate, bounds, n = 1000, evaluate = F))
+
+  } else if(is.list(gp)) {
+    gp_ptr = gp$pointer
+    da <- gp$priors
+  } else {
+    stop("GP should have a pointer and a priors object")
+  }
+  
+  ga <- garg(list(mle = T), y)
+  
+  mleGPsep(gp_ptr, param="d", tmin= da$min, tmax= da$max)  
+
+  ## optimization loop of sequential acquisitions
+  for(i in 1:n_iter) {
+    
+    # find best expected improvement
+    x_next <- EI.search(x, y, gp_ptr, bounds)
+    colnames(x_next) <- names(bounds)
+
+    # collect data at proposed location
+    y_next <- apply(x_next, 1, f, target = target, parameters = parameters)
+    
+    # update GP and length-scale
+    updateGPsep(gp_ptr, x_next, y_next)
+    mleGPsep(gp_ptr, param = "d", tmin = da$min, tmax = da$max, ab = da$ab)
+    
+    # save new simulation
+    x <- rbind(x, x_next)
+    y <- c(y, y_next)
+  }
+  
+
+  return(list(gp = list(pointer = gp_ptr, priors = da), 
+              evaluations = list(x = x, y = y),
+              minima = min(y)))
+}
+
+
+plot_preds <- function(gp_ptr, bounds, evals, par = 1, `3d` = FALSE) {
+  # initial fit
+  xx <- generate_range(bounds, n = 100)
+  
+  if(`3d`) {
+    XX <- expand.grid(xx[, 1], xx[, 2])
+    
+    pred <- predGPsep(gp_ptr, XX, lite=TRUE)
+    
+    z <- matrix(pred$mean, ncol = nrow(xx))
+    
+    persp(xx[, 1], xx[, 2], z, phi=45, theta=45,
+          xlab = "B_lf1", ylab = "hmat", zlab = "log(MAE)")
+  }
+  
+  pred <- predGPsep(gp_ptr, xx, lite=TRUE)
+  
+  plot(xx[, par], pred$mean, type = "l", ylim = c(0, max(evals$y)), 
+       xlab = "B_lf1", ylab = "log(MAE)")
+
+  lines(xx[, par], pred$mean + 1.96 * sqrt(pred$s2), col=2, lty=2)
+  lines(xx[, par], pred$mean - 1.96 * sqrt(pred$s2), col=2, lty=2)
+  
+  points(evals$x[, par], evals$y, col = "red")
+  
+}
--- scripts/optimise-biomass.R
+++ scripts/optimise-biomass.R
@@ -1,9 +1,11 @@
 devtools::load_all()
 library(tidyverse)
 
+source("scripts/optimise-bayesopt.R")
 
-# FF16
-base_mulga_parameters <- function() {
+
+# strategy defaults
+mulga <- function() {
   p0 <- scm_base_parameters("FF16", "FF16_Env")
   
   p0$strategy_default$lma <- 0.0645
@@ -17,28 +19,36 @@ base_mulga_parameters <- function() {
 }
 
 
+create_environment <- function() FF16_make_environment()
+create_site <- make_FF16_hyperpar
 
-set_patch <- function(traits =  trait_matrix(c(0.0645), c("lma")),
-                      B_lf1 = 1,
-                      seed_rain = 100,
-                      max_patch_lifetime = 250,
-                      p0 = base_mulga_parameters(),
-                      latitude = 28.182,
-                      schedule = NULL,
-                      optimise_schedule = FALSE,
-                      schedule_reduction_factor = 5) {
+create_species <- function(lma = 0.0645, hmat = 5, 
+                           birth_rate = 100,
+                           site) {
   
-  hyper_par_fn = make_FF16_hyperpar(B_lf1 = B_lf1, latitude = latitude)
-  p1 <- expand_parameters(traits, p0, hyper_par_fn, mutant = FALSE)
-  p1$seed_rain <- seed_rain
-  p1$max_patch_lifetime <- max_patch_lifetime
+  traits = trait_matrix(c(lma, hmat), c("lma", "hmat"))
+  sp = expand_parameters(traits, mulga(), site, mutant = FALSE)
   
-  if(!is.null(schedule)) {
-    p1$cohort_schedule_times = schedule
-  }
+  sp$birth_rate <- birth_rate
+  
+  return(sp)
+}
+
+# This is clunky - assumes one schedule will be suitable even while optimising
+# traits and site characteristics
+create_schedule <- function(max_patch_lifetime = 250,
+                            optimise_schedule = FALSE,
+                            schedule_reduction_factor = 5) {
+  
+  site <- create_site()
+  parameters <- create_species(site = site)
+  
+  # update patch longevity
+  parameters$max_patch_lifetime <- max_patch_lifetime
   
   if(optimise_schedule) {
-    # use built-in schedule
+
+    # start with built-in schedule
     nodes <- cohort_schedule_times_default(max_patch_lifetime)
     
     # then downsample, taking every nth integration node 
@@ -46,42 +56,45 @@ set_patch <- function(traits =  trait_matrix(c(0.0645), c("lma")),
       vector[c(1:starting_position, seq(starting_position, length(vector), n))] 
     }
     
-    p1$cohort_schedule_times[[1]] <- nth_element(nodes, schedule_reduction_factor)
+    times <- nth_element(nodes, schedule_reduction_factor)
     
-    result <- build_schedule(p1)
-  } else
-    result <- p1
+    parameters$cohort_schedule_times[[1]] <- times #1spp
+    
+    # and rebuild
+    parameters <- build_schedule(parameters)
+  }
 
-  return(result)
+  return(parameters)
 }
 
-net_biomass <- function(schedule, B_lf1) {
-  
-  p <- set_patch(schedule = schedule, 
-                 B_lf1 = B_lf1)
-  
+
+calculate_net_biomass <- function(parameters, environment) {
+
   # gather outputs at each time step
-  results <- run_scm_collect(p) %>% 
+  results <- run_scm_collect(parameters, environment) %>% 
     tidy_patch() %>%
     FF16_expand_state()
-
+  
   v <- c("mass_leaf", "mass_bark", "mass_sapwood", "mass_heartwood")
-
+  
   tissues <- results$species %>% 
     integrate_over_size_distribution() %>%
-    slice(-n()) %>% # drop last node
     select(time,species, one_of(v)) %>%
     pivot_longer(cols=starts_with("mass"), names_to = "tissue") %>%
     mutate(across(tissue, factor, levels = v)) %>%
-
+    filter(time != max(time)) # drop last node
+    
   return(tissues)
 }
 
 
-# Build schedule and visualise against tree yield formula
-p <- set_patch(optimise_schedule = T)
-tissues <- net_biomass(schedule = p$cohort_schedule_times,
-                       B_lf1 = 0.6931932)
+
+
+
+# Example -----------------------------------------------------------------
+
+
+p <- create_schedule(max_patch_lifetime = 50, optimise_schedule = T)
 
 tyf <- function(t, m = 50, g=12.25, r=1){
   k = 2 * g - 1.25
@@ -91,63 +104,58 @@ tyf <- function(t, m = 50, g=12.25, r=1){
   return(agb / 10) # t.ha-1 -> kg.m-2
 }
 
-ggplot(tissues, aes(time, value)) +
-  geom_area(aes(fill = tissue)) +
-  geom_function(fun=tyf, linetype = "dashed") +
-  labs(x = "Patch age (yr)", y = "Above ground mass (kg/m2)") +
-  theme_classic() + 
-  xlim(c(0,250)) +
-  facet_wrap(~species)
+# bounds = list(B_lf1 = list(min = 0.5, max = 1.5))
+
+bounds = list(B_lf1 = list(min = 0.5, max = 1.5),
+              hmat = list(min = 2, max = 10))
 
 
-# Optimising routines for B_lf1 search
-source("scripts/optimise-bayesopt.R")
+seed <- generate_seeds(calibrate, bounds, n = 6, target = tyf, parameters = p)
 
-calibrate <- function(x, biomass_fn = tyf, 
-                      schedule = p$cohort_schedule_times,
-                      n_control_pts = 50,
-                      match_tissue = "mass_heartwood",
-                      show_error = FALSE,
-                      ...) {
-  
-  tissues <- net_biomass(schedule, B_lf1 = x[1])
-  
-  # generate equidistant control points
-  s = schedule[[1]]
-  pts <- s[findInterval(seq(0, max(s), len = n_control_pts), s)]
-  
-  totals <- filter(tissues, 
-                   tissue == match_tissue, 
-                   time %in% pts) %>%
-    mutate(total_biomass = biomass_fn(time)) %>%
-    mutate(error = total_biomass - value)
-  
-  if(show_error)
-    plot(totals$time, totals$error)
-  
-  # loss fn
-  return(sum(abs(totals$error)))
-}
+fit <- optim.EI(calibrate, tyf, bounds, n_iter = 20, parameters = p, evals = seed)
+
+fit <- optim.EI(calibrate, tyf, bounds, n_iter = 20, parameters = p, 
+                evals = fit$evaluations,
+                gp = fit$gp)
+
+plot_preds(fit$gp$pointer, bounds, fit$evaluations, par = 1)
+
+deleteGPseps()
+
+# # optimise using expected improvement acquisition fn
+# fit <- bayesian_optimize(calibrate, bounds = c(0.5, 1))
 
 
-# optimise using expected improvement acquisition fn
-fit <- bayesian_optimize(calibrate, bounds = c(0.5, 1))
+# Test results
+m = which.min(fit$evaluations$y)
+
+fit$evaluations$x[m, ]
+
+site <- create_site(B_lf1 = fit$evaluations$x[m, 1], latitude = 28.182)
+sp <- create_species(hmat = fit$evaluations$x[m, 2], site = site)
+
+# sp <- create_species(site = site)
+
+sp$cohort_schedule_times <- p$cohort_schedule_times
+
+e <- create_environment()
 
 # re-run with current optima
-tissues <- net_biomass(schedule = p$cohort_schedule_times,
-                       B_lf1 =  fit$minima)
+tissues <- calculate_net_biomass(sp, e) %>%
+  filter(time != max(p$cohort_schedule_times[[1]]))
 
 ggplot(tissues, aes(time, value)) +
   geom_area(aes(fill = tissue)) +
   geom_function(fun=tyf, linetype = "dashed") +
   labs(x = "Patch age (yr)", y = "Above ground mass (kg/m2)") +
   theme_classic() + 
-  xlim(c(0,250)) +
   facet_wrap(~species)
- 
 
-# continue, re-using previous results
-fit <- bayesian_optimize(calibrate, 
-                         evals = fit$evaluations,
-                         iter = 5,
-                         bounds = c(0.5, 1))
+
+
+
+# # continue, re-using previous results
+# fit <- bayesian_optimize(calibrate, 
+#                          evals = fit$evaluations,
+#                          iter = 1,
+#                          bounds = c(0.5, 1))
