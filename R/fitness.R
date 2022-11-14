60e8b6de07beceaca3339c3a225e2c7ed04fec23
Daniel Falster <daniel.falster@unsw.edu.au>
2022-11-14 13:15:20 +1100

    Add missing tests for fitness & equilibrium functions (#355)
    * Missing tests on fitness
    * Rename test file so comes later in test routines
    
    Despite extensive tests, somehow various functions related to fitness and demographic equilibrium went untested. Presumably because they were slow. Lack of tests here enabled loss of functionality as we changed some of the interface.
    
    This PR adds some tests on master, which will then be ported back to develop and updated as needed.

--- R/fitness.R
+++ R/fitness.R
@@ -44,30 +44,3 @@ max_growth_rate <- function(trait_matrix, p) {
   fitness_landscape(trait_matrix, remove_residents(p))
 }
 
-##' Compute the carrying capacity (equilibrium per-capita seed
-##' production) for a set of values of a trait.  Each is considered in
-##' isolation.
-##'
-##' @title Carrying Capacity
-##' @param trait_matrix A matrix of traits
-##' @param p Parameters object to use.  Importantly, the
-##' \code{strategy_default} element gets used here.
-##' @param seed_rain Initial seed rain (optional)
-##' @param parallel Use multiple processors?
-##' @author Rich FitzJohn
-##' @export
-carrying_capacity <- function(trait_matrix, p, seed_rain=1,
-                              parallel=FALSE) {
-  f <- function(x) {
-    ## NOTE: This is not very pretty because matrix_to_list, which we
-    ## use to iterate over this, does not preserve array-ness or
-    ## names.
-    p <- expand_parameters(trait_matrix(x, traits),
-                           remove_residents(p),
-                           mutant=FALSE)
-    p$seed_rain <- seed_rain
-    equilibrium_seed_rain(p)$seed_rain
-  }
-  traits <- colnames(trait_matrix)
-  unlist(loop(matrix_to_list(trait_matrix), f, parallel=parallel))
-}
---
+++ tests/testthat/test-support-fitness.R
@@ -0,0 +1,219 @@
+context("fitness support")
+
+test_that("positive_1d", {
+  f <- function(x) -x^2 + 1
+  tol <- 1e-8
+  r <- positive_1d(f, 0, 0.1, tol=tol)
+  expect_lt(r[[1]], -1 + tol)
+  expect_gt(r[[2]], 1 - tol)
+
+  tol <- 1e-3
+  r <- positive_1d(f, 0, 0.1, tol=tol)
+  expect_lt(r[[1]], -1 + tol)
+  expect_gt(r[[2]], 1 - tol)
+
+  expect_error(positive_1d(f, -2, 0.1, tol=tol), "no positive values")
+})
+
+test_that("positive_2d", {
+  skip_if_no_plant_ml_python()
+  f <- function(x) {
+    if (!is.matrix(x)) {
+      x <- rbind(x, deparse.level=0)
+    }
+    -rowSums(x ^ 2) + 1
+  }
+
+  ans <- positive_2d(f, c(0, 0), -2, 2)
+})
+
+
+test_that("bounds", {
+
+  params <- scm_base_parameters("FF16")
+  params$strategy_default$hmat <- 0.5
+
+  # First test object with infiite bounds
+  expect_silent({ 
+    bounds0 <- bounds_infinite("lma")
+  })
+  expect_is(bounds0, "matrix")
+  expect_equal(bounds0,
+    matrix(c(-Inf, Inf), nrow=1, dimnames =list("lma",c("lower", "upper")))
+  )
+  
+  # Manually created bounds
+
+  expect_silent(
+    bounds_1d <- bounds(lma=c(0.01, 10))
+  )
+  expect_silent(
+    bounds_2d <- bounds(lma=c(0.01, 10), rho=c(1, 1000))
+  )
+
+  expect_is(bounds_1d, "matrix")
+  expect_equal(
+    bounds_1d,
+    matrix(c(0.01, 10), nrow = 1, dimnames = list("lma", c("lower", "upper")))
+  )
+
+  expect_is(bounds_2d, "matrix")
+  expect_equal(
+    bounds_2d,
+    matrix(c(0.01, 10, 1, 1000), byrow=TRUE, nrow = 2, dimnames = list(c("lma", "rho"), c("lower", "upper")))
+  )
+
+  expect_silent(
+    check_bounds(bounds_1d)
+  )
+  expect_silent(
+    check_bounds(bounds_2d)
+  )
+  
+  expect_error(
+    check_bounds(c(0,1))
+  )
+  expect_error(
+    check_bounds(matrix(c(0.01, 10, 1, 1000)))
+  )
+  expect_error(
+    check_bounds(bounds0, finite=TRUE)
+  )
+  expect_silent(
+    check_bounds(bounds0)
+  )
+  
+
+  # Points lie within bounds
+  expect_silent(
+    check_point(0.02, bounds_1d)
+  )
+  expect_error(
+    check_point(0.001, bounds_1d)
+  )
+})
+
+test_that("fitness", {
+  
+  # Now solve actual bounds for viable fitness
+  params <- scm_base_parameters("FF16")
+
+  expect_silent({ 
+    bounds1 <- viable_fitness(bounds_infinite("lma"), params)
+  })
+
+  expect_is(bounds1, "matrix")
+  expect_equal(
+      bounds1,
+      matrix(c(0.02533822, 4.989169), nrow = 1, dimnames = list("lma", c("lower", "upper")))
+  )
+  expect_silent({
+     bounds1b <- viable_fitness(bounds1, params)
+  })
+  expect_equal(
+    bounds1,
+    matrix(c(0.02533822, 4.989169), nrow = 1, dimnames = list("lma", c("lower", "upper"))), 
+    tolerance = 1e-4
+  )
+  # Now check edge cases
+
+  ## Incorrect inputs
+  expect_error(
+    bounds2 <- viable_fitness(NA, params)
+  )
+  expect_error(
+    bounds2 <- viable_fitness(c(-Inf, Inf), params)
+  )
+  ## starting point outside allowable area
+  expect_error(
+    bounds2 <- viable_fitness(bounds, x = -0.01, params)
+  )
+  ## 2D case currently failing
+  expect_error(
+    bounds2 <- viable_fitness(rbind(bounds0, bounds0, bounds0), params)
+  )
+
+  #@ max growth rate function
+  expect_silent(
+    fitness2 <- max_growth_rate(trait_matrix(0.05, "lma"), params)
+  )
+  expect_equal(
+    fitness2,
+    8.372336,
+    tolerance = 1e-4
+  )
+
+  # max fitness
+  expect_silent(
+    max_f <- max_fitness(bounds1, params, log_scale = TRUE)
+  )
+
+  expect_equal(max_f[1], 0.2985623, tolerance = 1e-4)
+  expect_equal(attr(max_f, "fitness"), 11.09135, tolerance = 1e-4)
+
+  # fitness landscape
+  lma <- trait_matrix(seq_log_range(bounds1, 5), "lma")
+
+  expect_silent({ 
+    fitness <- fitness_landscape(lma, params)
+  })
+
+  comparison <- c(-0.000510755977418667, 10.3682397084861, 11.0785948241787, 9.84121044132885, -0.000147021768340749)
+
+  expect_equal(
+    fitness, comparison, 
+    tolerance = 1e-4
+  )
+})
+
+# test_that("viable strategies", {
+#   params <- scm_base_parameters("FF16")
+#   patch <- expand_parameters(trait_matrix(c(0.005, 0.03, 0.1), "lma"), params, mutant = FALSE)
+
+#   expect_silent()
+#   check_inviable(patch)
+# })
+
+test_that("demographic equilibrium", {
+  params <- scm_base_parameters("FF16")
+  patch <- expand_parameters(trait_matrix(0.0825, "lma"), params, mutant = FALSE)
+  expect_silent(
+    patch_eq1 <- equilibrium_seed_rain(patch)
+  )
+  expect_true(attr(patch_eq1, "converged"))
+  expect_equal(attr(patch_eq1, "seed_rain_out"), 17.30373, tolerance = 1e-4)
+
+  p2 <- params
+  p2$control$equilibrium_solver_name <- "nleqslv"
+  patch <- expand_parameters(trait_matrix(0.0825, "lma"), p2, mutant = FALSE)
+  expect_silent(
+    patch_eq2 <- equilibrium_seed_rain(patch)
+  )
+  expect_true(attr(patch_eq2, "converged"))
+  expect_equal(attr(patch_eq2, "seed_rain_out"), 17.31023, tolerance = 1e-4)
+
+  p2$control$equilibrium_solver_name <- "hybrid"
+  patch <- expand_parameters(trait_matrix(0.0825, "lma"), p2, mutant = FALSE)
+  expect_silent(
+    patch_eq3 <- equilibrium_seed_rain(patch)
+  )
+  expect_true(attr(patch_eq3, "converged"))
+  expect_equal(attr(patch_eq3, "seed_rain_out"), 17.30623, tolerance = 1e-4)
+
+  p2 <- params
+  p2$control$equilibrium_solver_name <- "nonsense"
+  patch <- expand_parameters(trait_matrix(0.0825, "lma"), p2, mutant = FALSE)
+  expect_error(
+    patch_eq4 <- equilibrium_seed_rain(patch)
+  )
+
+  ## 2spp
+  patch <- expand_parameters(trait_matrix(c(0.0825, 0.2), "lma"), params, mutant = FALSE)
+  expect_silent(
+    patch_eq5 <- equilibrium_seed_rain(patch)
+  )
+  expect_true(attr(patch_eq5, "converged"))
+  expect_equal(attr(patch_eq5, "seed_rain_out"), c(12.41046, 13.93331), tolerance = 1e-4)
+
+
+})
