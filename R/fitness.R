971fb5303e4373ae7ea0346187726bb88fd810e0
Daniel Falster <daniel.falster@unsw.edu.au>
2023-10-24 11:26:39 +1100

    Remove comments

--- R/fitness.R
+++ R/fitness.R
@@ -22,18 +22,13 @@ fitness_landscape <- function(trait_matrix, p, hyperpar=param_hyperpar(p), log_f
   mutant_birth_rates <- rep(0, nrow(trait_matrix))
   p_mutants <- mutant_parameters(trait_matrix, p, hyperpar,
                                  birth_rate_list = mutant_birth_rates)
-  
+
   if (n_residents > 0L) {
     # if there's a resident, use the saved environment to calculate mutant fitness
     ctrl$save_RK45_cache = T  
-    
 
-    #p3 <- build_schedule(p, ctrl = ctrl)
-    #scm <- run_scm(p, ctrl = ctrl)
     scm <- run_scm(p, use_ode_times=length(p$node_schedule_ode_times) > 0, ctrl = ctrl)
-    #scm$net_reproduction_ratios
     scm$run_mutant(p_mutants)
-    #scm$net_reproduction_ratios
     net_reproduction_ratios <- scm$net_reproduction_ratios
   } else {
     # otherwise just run mutants with zero birth rate
@@ -41,7 +36,6 @@ fitness_landscape <- function(trait_matrix, p, hyperpar=param_hyperpar(p), log_f
                    ctrl = ctrl)
     net_reproduction_ratios <- scm$net_reproduction_ratios
   }
-
 
   if (log_fitness) {
     log(net_reproduction_ratios)
