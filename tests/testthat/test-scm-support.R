fd0bccf8523d02d8c0a11bb7591967bf3b672e53
Daniel Falster <daniel.falster@unsw.edu.au>
2025-08-05 17:05:24 +1000

    Remove reference now that tests are passing

--- tests/testthat/test-scm-support.R
+++ tests/testthat/test-scm-support.R
@@ -55,33 +55,13 @@ test_that("collect_auxiliary_variables", {
   p0 <- scm_base_parameters("FF16")
   p0$disturbance_mean_interval <- 30.0
   # two species
-  p2 <- expand_parameters(trait_matrix(c(0.0825, 0.2625), "lma"), p0, FF16_hyperpar,
-    birth_rate_list = list(11.99177, 16.51006)
+  p1 <- expand_parameters(trait_matrix(0.082, "lma"), p0, FF16_hyperpar,
+    birth_rate_list = list(11.99177)
   )
 
-  # Compare results to a reference of prior behavior. 
-  # Reference was caulcated from commit 4ca3a9be on develop, before simplifying interface for scm and collecting results
-  # The goal is to ensure consistent content of numerical outputs
-
-  results <- run_scm_collect(p2, env, ctrl)
-  #  saveRDS(results, "tests/testthat/test_data/run_collect_tidy_2spp.rds")
-  ref <- readRDS(file.path(rprojroot::find_testthat_root_file(), "test_data/run_collect_tidy_2spp.rds"))
+  results <- run_scm_collect(p1, env, ctrl)
   
   # check columns,should contain auxillary variables
   expect_equal(ncol(results$species), 15)
-  expect_contains(names(results$species), c("competition_effect", "net_mass_production_dt")) 
-  expect_contains(names(results), names(ref)[-1])
-  
-  expect_equal(results$steps$time, ref$time)
-  expect_equal(results$n_spp, ref$n_spp)
-  expect_equal(results$offspring_production, ref$offspring_production)
-  expect_equal(results$p, ref$p)
-  expect_equal(results$env$light_availability, ref$env$light_availability |> dplyr::select(names(results$env$light_availability)))
-
-  # Need to maniupulate species object for effective comnparison
-  v1 <- results$species |> dplyr::arrange(species, time, node) |> dplyr::filter(node != 142)
-  
-  v2 <- ref$species |>  tidyr::drop_na() |> dplyr::select(names(v1)) |>
-    dplyr::arrange(species, time, node) |> dplyr::slice(1:nrow(v1))  
-  expect_equal(v1, v2)
+  expect_contains(names(results$species), c("competition_effect", "net_mass_production_dt"))
 })
